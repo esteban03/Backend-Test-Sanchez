89a89a1c75a7f002f7a984f5ac080a67ed9a30fe
esteban.sanchez <esteban03@outloo.cl>
2020-06-02 17:24:40 -0400

    Test - Update test create menu

--- cornerapps/menu/tests/test_update_menu.py
+++ cornerapps/menu/tests/test_update_menu.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework.status import HTTP_200_OK
 
 from shared.tests import ApiTestCaseBase
-from cornerapps.menu.serializers import StoreMenuSerializer
+from cornerapps.menu.models import Menu, Option
 
 from faker import Faker
 
@@ -11,39 +11,47 @@ from faker import Faker
 class TestUpdateMenu(ApiTestCaseBase):
 
     def setUp(self):
-        self.route = reverse('menu:update')
         self.faker = Faker()
 
     def test_update_menu_success(self):
-        day_after_today = self.faker.date_this_month(before_today=False, after_today=True)
-
-        serializer = StoreMenuSerializer(data={
-            'day': day_after_today,
-            'options': [
-                "Tallerines con helado",
-                "Pure con papas fritas",
-                "Carne mechada con pure",
-            ]
-        })
-
-        serializer.is_valid()
-        menu = serializer.save()
-
-        menu_options = menu.options.all()
-
         user, token, credentials = self.generate_chef_user()
         self.set_client_credentials(token)
 
-        day_after_today_update = self.faker.date_this_month(before_today=False, after_today=True)
+        menu, options = self.create_menu(user)
 
+        route = reverse('menu:update', kwargs={'id': menu.id})
+
+        day_after_today_update = str(self.faker.future_date())
         update_menu = {'day': day_after_today_update, 'options': []}
 
-        for option in menu_options:
+        for option in options:
             update_menu['options'].append({
                 'id': option.pk,
                 'description': 'update food',
             })
 
-        response = self.client.post(self.route, update_menu)
+        response = self.client.put(route, update_menu, format='json')
 
         self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(response.data['day'], day_after_today_update)
+        self.assertEqual(
+            response.data['options'][0]['description'],
+            update_menu['options'][0]['description'],
+        )
+
+    def create_menu(self, user):
+        day_after_today = self.faker.future_date()
+        options = (
+            "Tallerines con helado",
+            "Pure con papas fritas",
+            "Carne mechada con pure",
+        )
+
+        menu = Menu.objects.create(user=user, day=day_after_today)
+
+        new_options = []
+        for option in options:
+            new_options.append(
+                Option.objects.create(menu=menu, description=option)
+            )
+        return menu, new_options
