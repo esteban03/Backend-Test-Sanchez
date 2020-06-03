549a176fc44c7e06fab04bb795b591a7c199e5ec
esteban.sanchez <esteban03@outloo.cl>
2020-06-03 17:00:46 -0400

    Delete unused folder in menu

--- cornerapps/menu/tasks_tmp/__init__.py
+++
@@ -1,2 +0,0 @@
-from .slack_reminder import send_slack_message
-from .imp_slack_reminder import send_message
--- cornerapps/menu/tasks_tmp/slack_reminder.py
+++
@@ -1,6 +0,0 @@
-from celery import shared_task
-from cornerapps.menu.tasks_tmp.imp_slack_reminder import send_message
-
-@shared_task
-def send_slack_message():
-    send_message()
