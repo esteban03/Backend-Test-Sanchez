69b51c4ca98e99ad913c2c4ebcde7d1844b73a74
esteban.sanchez <esteban03@outloo.cl>
2020-06-02 22:02:37 -0400

    Refactor - Delete unused import

--- cornerapps/menu/views/store_choose_menu.py
+++ cornerapps/menu/views/store_choose_menu.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import IsAuthenticated
 
 from cornerapps.user.permissions import IsEmployee
 from cornerapps.menu.serializers import ChooseMenuSerializer
-from cornerapps.menu.permissions import AuthorizedScheduleToChoose
 
 class StoreChooseMenu(CreateAPIView):
     permission_classes = (IsAuthenticated, IsEmployee, )
