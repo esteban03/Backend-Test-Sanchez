69333e390b66f8d0d07512f2ccbb28965acdb4ec
esteban.sanchez <esteban03@outloo.cl>
2020-06-02 22:01:16 -0400

    Fix - Add permissions IsChef in api index menu

--- cornerapps/menu/views/index_menu.py
+++ cornerapps/menu/views/index_menu.py
@@ -1,8 +1,12 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+
 from cornerapps.menu.models import Menu
 from cornerapps.menu.serializers import IndexMenuModelSerializer
+from cornerapps.user.permissions import IsChef
 
 
 class IndexMenu(ListAPIView):
+    permission_classes = (IsAuthenticated, IsChef, )
     queryset = Menu.objects.all()
     serializer_class = IndexMenuModelSerializer
