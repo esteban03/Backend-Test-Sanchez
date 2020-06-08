f9154e4c5ab60cce83588e77d9ad0a2f18584970
esteban.sanchez <esteban03@outloo.cl>
2020-06-07 21:06:24 -0400

    Feat - Add filter day menu

--- cornerapps/menu/views/index_menu.py
+++ cornerapps/menu/views/index_menu.py
@@ -5,8 +5,12 @@ from cornerapps.menu.models import Menu
 from cornerapps.menu.serializers import IndexMenuModelSerializer
 from cornerapps.user.permissions import IsChef
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class IndexMenu(ListAPIView):
     permission_classes = (IsAuthenticated, IsChef, )
     queryset = Menu.objects.all()
     serializer_class = IndexMenuModelSerializer
+    filter_backends = (DjangoFilterBackend, )
+    filterset_fields = ('day', )
